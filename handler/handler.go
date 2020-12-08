91dd784ef9670805ee88e04b336f52d41934fabd
Sirok47 <xromosoma.47-maya@yandex.by>
2020-12-08 13:13:59 +0300

    dbd

--- handler/handler.go
+++ handler/handler.go
@@ -13,21 +13,25 @@ type TopGames struct {
 }
 
 func (con *TopGames) ReadLine(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
+	id, err := strconv.Atoi(c.Param("id"))
+	if err!=nil{return c.String(500, "ID can't be converted")}
 	return c.JSON(200, con.Service.ReadLine(id))
 }
 func (con *TopGames) CreateLine(c echo.Context) error {
 	g := model.SingleGame{}
-	g.Id,_ = strconv.Atoi(c.Param("id"))
-	g.Name = c.Param("name")
-	rr, _ := strconv.Atoi(c.Param("rating"))
+	rr,err := strconv.Atoi(c.Param("rating"))
+	if err!=nil{return c.String(500, "Rating can't be converted")}
 	g.Rating=float64(rr)
+	g.Id,err = strconv.Atoi(c.Param("id"))
+	if err!=nil{return c.String(500, "ID can't be converted")}
+	g.Name = c.Param("name")
 	g.Platform= c.Param("platform")
 	g.Date= c.Param("date")
-	return c.String(200, con.Service.CreateLine(&g))
+	return c.String(201, con.Service.CreateLine(&g))
 }
 func (con *TopGames) DeleteLine(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
+	id, err := strconv.Atoi(c.Param("id"))
+	if err!=nil{return c.String(500, "ID can't be converted")}
 	return c.String(200, con.Service.DeleteLine(id))
 }
 
