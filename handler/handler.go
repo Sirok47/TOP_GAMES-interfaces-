094f57e22fbeb1200f9a1fc42a2ea1d1d95d0708
Sirok47 <75079540+Sirok47@users.noreply.github.com>
2020-12-22 12:23:51 +0300

    Add files via upload

--- handler/handler.go
+++ handler/handler.go
@@ -5,6 +5,9 @@ import (
 	"context"
 	"net/http"
 	"strconv"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 
 	grpcpb "github.com/Sirok47/TOP_GAMES-interfaces-/grpc"
 
@@ -15,12 +18,13 @@ import (
 
 // TopGames stores DB connection's, context's and next structure's objects for handler package
 type TopGames struct {
-	cli grpcpb.CRUDClient
+	cli   grpcpb.CRUDClient
+	users map[string]string
 }
 
 // NewHandler is a constructor for creating "TopGames"'s object in handler package
-func NewHandler(cli grpcpb.CRUDClient) *TopGames {
-	return &TopGames{cli}
+func NewHandler(cli grpcpb.CRUDClient, u map[string]string) *TopGames {
+	return &TopGames{cli, u}
 }
 
 // Read gets id from request and passes in to srv.Read
@@ -41,7 +45,6 @@ func (con *TopGames) Read(c echo.Context) error {
 // Create decodes JSON request into "TopGames"'s object and passes it to srv.Create
 func (con *TopGames) Create(c echo.Context) error {
 	g := new(model.SingleGame)
-
 	if err := c.Bind(g); err != nil {
 		return errors.Wrap(err, "Wrong input")
 	}
@@ -79,4 +82,23 @@ func (con *TopGames) Delete(c echo.Context) error {
 		return c.String(http.StatusInternalServerError, err2.Err)
 	}
 	return c.String(http.StatusOK, "Line have been deleted")
+}
+
+func (con *TopGames) Login(c echo.Context) error {
+	username := c.Param("name")
+	password := c.Param("pass")
+	if con.users[username] == password {
+		token := jwt.New(jwt.SigningMethodHS256)
+		claims := token.Claims.(jwt.MapClaims)
+		claims["name"] = "Max"
+		claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+		t, err := token.SignedString([]byte("sirok"))
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, map[string]string{
+			"token": t,
+		})
+	}
+	return echo.ErrUnauthorized
 }
--- main.go
+++ main.go
@@ -4,25 +4,32 @@ import (
 	grpcpb "github.com/Sirok47/TOP_GAMES-interfaces-/grpc"
 	"github.com/Sirok47/TOP_GAMES-interfaces-/handler"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	_ "github.com/lib/pq"
 	_ "go.mongodb.org/mongo-driver/mongo/readpref"
 	"google.golang.org/grpc"
 )
 
 func main() {
+	users := map[string]string{"max": "5742368M"}
+	LoggedIn := middleware.JWTWithConfig(middleware.JWTConfig{
+		SigningKey: []byte("sirok"),
+	})
 	gcon, _ := grpc.Dial(":8080", grpc.WithInsecure())
 	cli := grpcpb.NewCRUDClient(gcon)
-	con := handler.NewHandler(cli)
+	con := handler.NewHandler(cli, users)
 
 	e := echo.New()
 
-	e.POST("/write", con.Create)
+	e.POST("/login/:name/:pass", con.Login)
 
-	e.DELETE("/delete/:id", con.Delete)
+	e.POST("/write", con.Create, LoggedIn)
 
-	e.GET("/read/:id", con.Read)
+	e.DELETE("/delete/:id", con.Delete, LoggedIn)
 
-	e.PUT("/update", con.Update)
+	e.GET("/read/:id", con.Read, LoggedIn)
+
+	e.PUT("/update", con.Update, LoggedIn)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
